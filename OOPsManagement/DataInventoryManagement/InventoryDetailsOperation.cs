3a6f799ea27172ff3506bf13175f2b2ba9ff3bc8
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-19 16:45:03 +0530

    [Riya] Add : JSON Inventory Data Management

---
+++ OOPsManagement/DataInventoryManagement/InventoryDetailsOperation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace OOPsManagement.DataInventoryManagement
+{
+    public class InventoryDetailsOperation
+    {
+        public void ReadInventoryJson(string filePath)
+        {
+            var json =File.ReadAllText(filePath);
+            List<InventoryDetails> list = JsonConvert.DeserializeObject<List<InventoryDetails>>(json);
+            foreach (var data in list)
+            {
+                Console.WriteLine("Name:" + data.Name + " Weight:" + data.Weight + " PricePerKg:" + data.PricePerKg);
+            }
+        }
+    }
+}
--- OOPsManagement/Program.cs
+++ OOPsManagement/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using OOPsManagement.DataInventoryManagement;
+using System;
+
+namespace OOPsManagement
+{
+    class Program
+    {
+        static string DataInventoryManagement_filePath = @"D:\BridgeLabs Training\OOPsManagement\OOPsManagement\DataInventoryManagement\InventoryData.json";
+        public static void Main(string[] args)
+        {
+            InventoryDetailsOperation detailsOperation = new InventoryDetailsOperation();
+            detailsOperation.ReadInventoryJson(DataInventoryManagement_filePath);
+        }
+    }
+}
