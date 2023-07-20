c5e1a31831f902c89f7ed106746d3e6c3f3e8ecd
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-20 13:04:12 +0530

    [Riya] Add : Stock Account Management Program

---
+++ OOPsManagement/StockAccountManagement/StockOperation.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs.StockAccountManagement
+{
+    public class StockOperation
+    {
+        public void ReadInventoryJson(string filePath)
+        {
+            var json = File.ReadAllText(filePath);
+            List<Stock> list = JsonConvert.DeserializeObject<List<Stock>>(json);
+            foreach (var data in list)
+            {
+                Console.WriteLine(data.StockName + " " + data.NoOfShares + " " + data.SharePrice);
+            }
+        }
+    }
+}
